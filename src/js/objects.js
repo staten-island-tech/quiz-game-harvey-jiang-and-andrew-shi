39163869c8520e7864349dfc270316b915030db1
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-04-10 21:03:52 -0400

    asdasd

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,7 +1,7 @@
 const quizQuestions = [
   {
     img:
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66439990-408d-44fc-988a-2375b88d6183/de7cg0p-e2dcc2c3-b548-4c51-808e-b44efc8ca690.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjY0Mzk5OTAtNDA4ZC00NGZjLTk4OGEtMjM3NWI4OGQ2MTgzXC9kZTdjZzBwLWUyZGNjMmMzLWI1NDgtNGM1MS04MDhlLWI0NGVmYzhjYTY5MC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.7Qe7SGvhwWqLbrh5ALVBTn1a-lDJNh0v1IbDinvuBy0",
+      '<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66439990-408d-44fc-988a-2375b88d6183/de7cg0p-e2dcc2c3-b548-4c51-808e-b44efc8ca690.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjY0Mzk5OTAtNDA4ZC00NGZjLTk4OGEtMjM3NWI4OGQ2MTgzXC9kZTdjZzBwLWUyZGNjMmMzLWI1NDgtNGM1MS04MDhlLWI0NGVmYzhjYTY5MC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.7Qe7SGvhwWqLbrh5ALVBTn1a-lDJNh0v1IbDinvuBy0" id="img" class="figure-img img-fluid" alt="sumi">',
     question: "who is this girl",
     answersChoices: [
       { a: "Chizuru Ichinose" },
@@ -12,7 +12,9 @@ const quizQuestions = [
     correctAnswer: "Sumi Sakurasawa",
   },
   {
-    img: "https://i.redd.it/loerq2qqr4n51.png",
+    img:
+      '<img src="https://i.redd.it/loerq2qqr4n51.png" id="img" class="figure-img img-fluid" alt="sumi">',
+
     question: "any cuties AYAYA?",
     answersChoices: [
       { a: "Wosaodjka" },
@@ -24,7 +26,8 @@ const quizQuestions = [
   },
 
   {
-    img: "https://i.redd.it/mnxufx3rd6l51.jpg",
+    img:
+      '<img src="https://upload.wikimedia.org/wikipedia/en/9/95/BCS4KeyVisual.jpg" id="img" class="figure-img img-fluid" alt="sumi">',
     question: "So many Cuties in chat today EHEHE?",
     answersChoices: [
       { a: "asdasda" },
@@ -38,7 +41,7 @@ const quizQuestions = [
 
 const DOMselectors = {
   quizQuestion: document.querySelector("#question"),
-  quizImg: document.querySelector("#img"),
+  quizImg: document.querySelector(".figure"),
   displayContainer: document.querySelector("#question-container"),
   startBtn: document.querySelector("#start-quiz-btn"),
 
